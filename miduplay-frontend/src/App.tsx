d7290dd90de7617015056033a14804e682a89b6b
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2025-05-20 23:05:19 +0530

    setup shadcn

--- miduplay-frontend/src/App.tsx
+++ miduplay-frontend/src/App.tsx
@@ -1,72 +1,74 @@
 import axios from "axios";
 import React from "react";
-import { SignedIn, SignedOut, SignInButton, useAuth, UserButton } from '@clerk/clerk-react';
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  useAuth,
+  UserButton,
+} from "@clerk/clerk-react";
+import { Button } from "./components/ui/button";
+import Header from "./components/common/Header";
 
 const App = () => {
   const [selectedFile, setSelectedFile] = React.useState<File | null>(null);
 
   const fileChangeHandler = async (file: File) => {
     console.log("file", file);
-    setSelectedFile(file)
-    
+    setSelectedFile(file);
   };
 
-   const { getToken } = useAuth()
+  const { getToken } = useAuth();
   const uploadHandler = async () => {
     console.log("import.meta.env.VITE_API_URL", import.meta.env.VITE_API_URL);
-    if (!selectedFile) {  
-      console.error("No file selected");
-      return;
-    }
-
-    const token = await getToken();
-    console.log("token", token);
-
-
-    const response = await axios.post(
-      `${import.meta.env.VITE_API_URL}/get-presigned-url`,
-      {
-        fileName: selectedFile.name,
-        fileType: selectedFile.type,
-      },
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
+    try {
+      if (!selectedFile) {
+        console.error("No file selected");
+        return;
       }
-    );
-    console.log("response", response);
 
-    const { url, filePath } = response.data;
+      const token = await getToken();
+      console.log("token", token);
 
-    const uploadResponse = await axios.put(url, selectedFile, {
-      headers: {
-        "Content-Type": selectedFile.type,
-      },
-      onUploadProgress: (progress) => {
-        console.log("progress", progress);
-      },
-    });
+      const response = await axios.post(
+        `${import.meta.env.VITE_API_URL}/get-presigned-url`,
+        {
+          fileName: selectedFile.name,
+          fileType: selectedFile.type,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      console.log("response", response);
 
-    console.log("uploadResponse", uploadResponse);
-    if (uploadResponse.status === 200) {
-      console.log("File uploaded successfully");
-    } else {
-      console.error("File upload failed");
+      const { url, filePath } = response.data;
+
+      const uploadResponse = await axios.put(url, selectedFile, {
+        headers: {
+          "Content-Type": selectedFile.type,
+        },
+        onUploadProgress: (progress) => {
+          console.log("progress", progress);
+        },
+      });
+
+      console.log("uploadResponse", uploadResponse);
+      if (uploadResponse.status === 200) {
+        console.log("File uploaded successfully");
+      } else {
+        console.error("File upload failed");
+      }
+    } catch (error) {
+      console.error("Error getting token", error);
     }
   };
   return (
     <div className="flex flex-col items-center mt-3 h-screen">
-         <header>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </header>
-
+      <Header />
       <div className="flex flex-col items-center mt-5">
         <h2 className="text-2xl font-semibold">Upload video</h2>
         <input
@@ -80,7 +82,7 @@ const App = () => {
             }
           }}
         />
-        <button onClick={uploadHandler}>Upload</button>
+        <Button onClick={uploadHandler}>Upload</Button>
       </div>
     </div>
   );
