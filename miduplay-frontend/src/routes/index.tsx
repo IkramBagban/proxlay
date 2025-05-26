025420984b9ba13e8b71fd643c709cf246a7cc11
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2025-05-26 17:08:54 +0530

    remove comment

--- miduplay-frontend/src/routes/index.tsx
+++ miduplay-frontend/src/routes/index.tsx
@@ -31,27 +31,3 @@ const RootRouter = () => {
 };
 
 export default RootRouter;
-
-// import App from "@/App";
-// import SpacesPage from "@/app/workspace/page";
-// import { Route, Routes } from "react-router";
-// import WorkspaceDetails from "@/app/workspace/workspace-details/page";
-// import YoutubeVideoUpload from "@/app/platforms/youtube/upload-video/page";
-
-// const RootRouter = () => {
-//   return (
-//     <Routes>
-//       <Route path="/" element={<App />} />
-//       <Route path="/workspace">
-//         <Route index element={<SpacesPage />} />
-//         <Route path="/workspace/:workspaceId" element={<WorkspaceDetails />} />
-//         <Route
-//           path="/workspace/:workspaceId/youtube/upload"
-//           element={<YoutubeVideoUpload />}
-//         />
-//       </Route>
-//     </Routes>
-//   );
-// };
-
-// export default RootRouter;
